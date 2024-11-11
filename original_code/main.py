c5f84b4ae1883941c9bca4976d284b12f4e3e749
kitazb <kitazb@berea.edu>
2024-11-11 10:42:58 -0500

    Started to write code for database

--- original_code/main.py
+++ original_code/main.py
@@ -5,8 +5,12 @@ class User:
     def __init__(self):
         self.name = self.retrieve_name()
         self.pin = self.retrieve_pin()
+        self.con = sqlite3.connect("registration.db")
+        self.cursor = self.con.cursor()
+
 
     def retrieve_name(self):
+        self.cursor()
         return ""
 
     def retrieve_pin(self):
@@ -21,3 +25,12 @@ class User:
     def retrieve_course(self):
         pass
 app = Flask(__name__)
+
+def creat_table():
+    create_table_query = '''
+    CREATE TABLE IF NOT EXISTS Users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        pin INTEGER NOT NULL,
+    )
+    '''
