bbe193688dcc8e52f63ab7e5794af9053748573e
Aliaksandr Melnichenka <togpe22@gmail.com>
2024-11-20 22:30:22 -0500

    Initial code commits

---
+++ image_processor.py
@@ -0,0 +1,23 @@
+######################################################################
+# Author: Aliaksandr Melnichenka, Denys Zhytkov        TODO: Change this to your names
+# Username: melnichenkaa_zhytkovd
+#
+# Assignment: P01: Final Project
+#
+#
+# Purpose: Learn about classes, inheritance, and Pygame
+######################################################################
+# Acknowledgements:
+#
+#
+# MIT Licence
+####################################################################################
+
+# import pygame
+
+import os
+from flask import Flask, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+
+# if __name__ == '__main__':
+    # app.run(debug=True)
