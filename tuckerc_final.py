3c36bfe47097c0d48f3885977e6652f5b8753960
tuckercberea <tuckerc@berea.edu>
2024-11-20 23:55:46 -0500

    Created planner outline

--- tuckerc_final.py
+++ tuckerc_final.py
@@ -13,3 +13,24 @@
 ####################################################################################
 
 import tkinter as tk
+from tkinter import messagebox
+
+class CoursePlanner:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Course Planner")
+        self.root.geometry("700x400")
+
+    def create_calendar(self):
+        days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+    def save_schedule(self, day, time, value, event):
+
+    def display_schedule(self):
+        schedule_display = ""
+        messagebox.showinfo("Your Schedule", schedule_display)
+
+root = tk.Tk()
+planner = CoursePlanner(root)
+
+root.mainloop()
