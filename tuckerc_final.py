0bb884150ae07a03385c8f0427e44ae27b73fa5a
tuckercberea <tuckerc@berea.edu>
2024-11-20 22:46:28 -0500

    Created file header and imported tk

---
+++ tuckerc_final.py
@@ -0,0 +1,15 @@
+######################################################################
+# Author: Caleb Tucker
+# Username: tuckerc
+#
+# T12: Events and GUIs
+#
+# Purpose: To create an interactive calendar
+#######################################################################
+# Acknowledgements:
+#
+# licensed under a Creative Commons
+# Attribution-Noncommercial-Share Alike 3.0 United States License.
+####################################################################################
+
+import tkinter as tk
